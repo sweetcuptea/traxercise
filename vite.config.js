aa60ecc7ac42820f2056e9c0abb756bfd5ae715d
sweetcuptea <sweetcuptea@gmail.com>
2023-03-25 08:58:10 +0700

    init vue cli

---
+++ vite.config.js
@@ -0,0 +1,40 @@
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import legacy from "@vitejs/plugin-legacy";
+import vue2 from "@vitejs/plugin-vue2";
+import vue2Jsx from "@vitejs/plugin-vue2-jsx";
+import { type } from "node:os";
+
+// dependencies
+import fonts from "vite-plugin-fonts";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue2(),
+    vue2Jsx(),
+    legacy({
+      targets: ["ie >= 11"],
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+    }),
+    fonts({
+      dirs: ["./src/assets/fonts"],
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "./src/assets/scss/fonts.scss";
+          @import "./src/assets/scss/variable.scss";
+          @import "./src/assets/scss/reset.scss";
+        `,
+      },
+    },
+  },
+});
