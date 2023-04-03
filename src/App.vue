e8c202d28c60339941ac8132691289b0f0f43385
sweetcuptea <sweetcuptea@gmail.com>
2023-04-03 12:24:53 +0700

    create a preloader

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,41 @@
 <template>
   <div id="app">
-    <h1>hello world</h1>
+    <tx-preloader v-if="preload" :progress="progress" />
+
+    <main v-else>
+      <h1>hello world</h1>
+    </main>
   </div>
 </template>
 
 <script>
-export default {}
+import Preloader from "./assets/components/atoms/Preloader.vue";
+
+export default {
+  components: {
+    "tx-preloader": Preloader,
+  },
+  data() {
+    return {
+      preload: true,
+      progress: 0,
+    };
+  },
+  mounted() {
+    const preloadInterval = setInterval(() => {
+      if (this.progress < 100) {
+        this.progress++;
+      } else {
+        clearInterval(preloadInterval);
+        this.preload = false;
+      }
+    }, 10);
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+main {
+  margin: $spacing-800 $spacing-400 0;
+}
 </style>
---
+++ src/assets/components/atoms/Preloader.vue
@@ -0,0 +1,46 @@
+t
+<template>
+  <div class="progress-bar-container">
+    <div class="progress-bar">
+      <div class="loader" :style="{ width: progress + '%' }"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Preloader",
+  data() {
+    return {};
+  },
+  props: {
+    progress: {
+      type: Number,
+      require: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.progress-bar-container {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .progress-bar {
+    width: 30vw;
+    height: 8px;
+    background-color: $dark-color-300;
+    border-radius: $radius-100;
+
+    .loader {
+      height: 8px;
+      background-color: $accent-color-100;
+      border-radius: $radius-100;
+    }
+  }
+}
+</style>
