737b3a7774fb0bb3b8291707ff5b4ba9a69d56eb
sweetcuptea <105599657+sweetcuptea@users.noreply.github.com>
2023-04-04 22:13:57 +0700

    create a preloader (#7)
    * create a preloader
    
    * fixing-the-naming-for-some-codes-and-changing-the-folder-structure

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,41 @@
 <template>
   <div id="app">
-    <h1>hello world</h1>
+    <TxPreloader v-if="isLoading" :progress="progress" />
+
+    <main class="main" v-else>
+      <h1>hello world</h1>
+    </main>
   </div>
 </template>
 
 <script>
-export default {}
+import TxPreloader from "./components/atoms/TxPreloader.vue";
+
+export default {
+  components: {
+    TxPreloader,
+  },
+  data() {
+    return {
+      isLoading: true,
+      progress: 0,
+    };
+  },
+  mounted() {
+    const preloadInterval = setInterval(() => {
+      if (this.progress < 100) {
+        this.progress++;
+      } else {
+        clearInterval(preloadInterval);
+        this.isLoading = false;
+      }
+    }, 10);
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+.main {
+  margin: $spacing-800 $spacing-400 0;
+}
 </style>
---
+++ src/components/atoms/TxPreloader.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="preloader-container">
+    <div class="preloader-bar">
+      <div class="preloader-fill" :style="{ width: progress + '%' }"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TxPreloader",
+  data() {
+    return {};
+  },
+  props: {
+    progress: {
+      type: Number,
+      require: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.preloader-container {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .preloader-bar {
+    width: 30vw;
+    height: 8px;
+    background-color: $dark-color-300;
+    border-radius: $radius-100;
+
+    .preloader-fill {
+      height: 8px;
+      background-color: $accent-color-100;
+      border-radius: $radius-100;
+    }
+  }
+}
+</style>
