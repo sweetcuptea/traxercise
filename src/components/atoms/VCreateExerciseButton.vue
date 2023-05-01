dc67ff534236557a71637c90d115b96affe2b212
sweetcuptea <105599657+sweetcuptea@users.noreply.github.com>
2023-05-01 17:38:08 +0700

    make a component to begin to create an exercise (#13)
    * make a component to begin to create exercise
    
    * Update src/components/atoms/VCreateExerciseButton.vue
    
    Co-authored-by: Tri Hargianto <trihargianto@gmail.com>
    
    * applying alternative code for css hover and active effects
    
    ---------
    
    Co-authored-by: Tri Hargianto <trihargianto@gmail.com>

---
+++ src/components/atoms/VCreateExerciseButton.vue
@@ -0,0 +1,47 @@
+<template>
+  <router-link to="" exact>
+    <button class="btn-create-exercise">
+      <v-plus-icon class="btn-create-exercise__icon" />
+    </button>
+  </router-link>
+</template>
+
+<script>
+import { VPlusIcon } from "./IconComponents";
+
+export default {
+  name: "VCreateExerciseButton",
+  components: {
+    VPlusIcon,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.btn-create-exercise {
+  width: auto;
+  height: auto;
+  position: fixed;
+  right: 36px;
+  bottom: 100px;
+  background: none;
+  border: none;
+  outline: none;
+  z-index: 9999;
+
+  &__icon {
+    width: 40px;
+    height: 40px;
+    padding: 8px;
+    color: $dark-color-100;
+    background-color: $accent-color-100;
+    border-radius: 100%;
+    cursor: pointer;
+
+    &:hover,
+    &:active {
+      background-color: $accent-color-200;
+    }
+  }
+}
+</style>
